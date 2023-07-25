5bd2380a024914cc1b00deff38eabb799763a093
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2023-07-25 14:16:04 -0300

    Also add symlink

--- i18n_pages.rb
+++ i18n_pages.rb
@@ -1,7 +1,7 @@
 require 'optparse'
+require 'yaml'
 
 LANGUAGES = ['en']
-COLLECTIONS = ['meetups']
 
 options = {}
 OptionParser.new do |opts|
@@ -22,6 +22,9 @@ pages = files.select { |file|
   file.match?(%r{(?:^_includes/)|(?:^_layouts/)|(?:^_site/)|(?:^README)|(?:^404)|(?:^en/)})
 }
 
+config = YAML.safe_load_file("_config.yml")
+collections = config["collections"].reject { |key, _| key.end_with?('_en') }
+
 pages.each do |page|
   LANGUAGES.each do |language|
     page_dir = File.dirname(page)
@@ -34,10 +37,21 @@ pages.each do |page|
     content.gsub!(%r{href=(.)/}, "href=\\1/#{language}/")
 
     # i18n collections
-    COLLECTIONS.each do |collection|
+    collections.each do |collection|
       content.gsub!(%r{site.#{collection}}, "site.#{collection}_#{language}")
     end
 
     File.write("#{language}/#{page}", content)
   end
 end
+
+collections.each do |key, value|
+  LANGUAGES.each do |language|
+    next unless value["permalink"]
+
+    config["collections"]["#{key}_#{language}"] = value.merge("permalink" => "/#{language}#{value["permalink"]}")
+    File.symlink("#{language}/_#{key}", "_#{key}_#{language}") unless File.exist?("_#{key}_#{language}")
+  end
+end
+
+File.open('_config.yml', 'w') {|f| f.write config.to_yaml }
