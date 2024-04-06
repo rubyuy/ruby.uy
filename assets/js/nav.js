31132f560acaba6d9687cfd66785f6b0f0594887
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2024-04-05 23:15:16 -0300

    Initial implementation of next meetup section

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,3 +3,20 @@ function toggleNavLinks() {
     link.style.display = link.style.display === "flex" ? "none" : "flex"
   })
 }
+
+function duplicate(element, {times}) {
+  for (let i = 0; i < times; i++) {
+    const clone = element.cloneNode(true)
+    clone.setAttribute("aria-hidden", "true")
+    element.parentNode.appendChild(clone)
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const viewportWidth = document.documentElement.clientWidth || window.innerWidth;
+
+  if (viewportWidth > 768) {
+    const element = document.querySelector(".example")
+    duplicate(element, {times: 6})
+  }
+})
