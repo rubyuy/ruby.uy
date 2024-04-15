8483d98643bb4271a34042146a9184be55eda5a6
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2024-04-15 01:59:18 -0300

    Make next meetup callout animated and add address_url for companies and use it for next meetup callout

---
+++ assets/js/next_meetup_callout.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const nextMeetupCallout = document.querySelector(".next-meetup-callout")
+  const nextMeetupCalloutTitle = document.querySelector(".next-meetup-callout__title")
+  const nextMeetupCalloutIcon = document.querySelector(".next-meetup-callout__icon")
+
+  for(let i = 0; i < 5; i++) {
+    duplicate(nextMeetupCalloutTitle)
+    duplicate(nextMeetupCalloutIcon)
+  }
+
+  duplicate(nextMeetupCallout)
+  addAnimationToCallouts()
+})
+
+function addAnimationToCallouts() {
+  document.querySelectorAll(".next-meetup-callout").forEach(callout => {
+    callout.classList.add("next-meetup-callout--infinite-horizontal-animation")
+  })
+}
+
+function duplicate(element, {times: times = 1} = {}) {
+  for (let i = 0; i < times; i++) {
+    const clone = element.cloneNode(true)
+    clone.setAttribute("aria-hidden", "true")
+    element.parentNode.appendChild(clone)
+  }
+}
